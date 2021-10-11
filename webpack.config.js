40d2203815e1a47329b6156fcf7d4758cb49613c
VaporousChain6 <pablommruiz04@gmail.com>
2021-10-11 11:46:13 +0200

    Configuration set

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path');
+module.exports = {
+  mode: "production",
+
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+
+    static: './dist',
+
+  },
+  plugins: [
+
+
+  ],
+};
